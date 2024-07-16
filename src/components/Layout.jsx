d0920a82b97c931aceb8281e5d02b17baffc2771
Naveen bandela <raramuri@172-0-65-194.lightspeed.miamfl.sbcglobal.net>
2024-07-16 15:54:16 +0530

    added animation in gihub star icon

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -23,7 +23,7 @@ function Header() {
   let [isScrolled, setIsScrolled] = useState(false)
   let pathname = usePathname()
   const isCertificate = pathname.includes('certificate')
-  const [githubStars, setGithubStars] = useState(1300)
+  const [githubStars, setGithubStars] = useState(null)
 
   useEffect(() => {
     const fetchRepoData = async () => {
@@ -92,21 +92,25 @@ function Header() {
             <div className="relative flex items-center justify-center space-x-1 overflow-hidden rounded-xl px-2 py-1">
               <GitHubIcon className="mr-1 h-5 w-5 fill-slate-400 group-hover:fill-slate-500 dark:group-hover:fill-slate-300" />
 
-              <span className=" text-sm font-semibold text-white">
-                {formatNumber(githubStars)}
-              </span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="#fcd34d"
-                className="h-3.5 w-3.5 [--icon-background:theme(colors.white)] [--icon-foreground:theme(colors.slate.900)]"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
-                  clipRule="evenodd"
-                />
-              </svg>
+              {githubStars && (
+                <div className="animate-fadeIn flex items-center gap-1">
+                  <span className=" text-sm font-semibold text-white">
+                    {formatNumber(githubStars)}
+                  </span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="#fcd34d"
+                    className="h-3.5 w-3.5 [--icon-background:theme(colors.white)] [--icon-foreground:theme(colors.slate.900)]"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </div>
+              )}
             </div>
           </div>
         </Link>
