a4679d6ce01896310839615a9561fd6a4a39c7a5
Andrew William Staines <raramuri@apples-MacBook-Pro.local>
2024-08-02 20:01:57 +0530

    Solved the issue of going top in Features tab

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -10,37 +10,31 @@ export function HomePage() {
           title="Battle Tested at Enterprise Scale"
           description="Built over years running production workload at great scales in variety of industries."
           icon="presets"
-          href="#"
         />
         <QuickLink
           title="Out of the box Observability"
           description="Goodness of Metrics, Traces and Logs without a single line of code. Traces to OpenTelemetry compatible providers, Metrics in Prometheus Format."
           icon="lightbulb"
-          href="#"
         />
         <QuickLink
           title="REST Standards by default"
           description="Default behaviour is how 'you' would design a REST API. Status codes, response formats - all taken care by GoFr automagically."
           icon="plugins"
-          href="#"
         />
         <QuickLink
           title="Middleware support"
           description="Elevate productivity effortlessly with predefined middleware's, while retaining flexibility through seamless integration of custom middleware tailored to your specific needs."
           icon="middleware"
-          href="#"
         />
         <QuickLink
           title="Environment based config"
           description="Following the 12-factor config principles for maintaining application configurations, simplify the integration of data sources like MySQL, Postgres, Kafka, Google Pubsub, and others."
           icon="env"
-          href="#"
         />
         <QuickLink
           title="Crash Handling"
           description="GoFr catches all and every panics and automatically recovers from them to maintain continuous availability of your server."
           icon="crash"
-          href="#"
         />
       </div>
       <div className="mx-4 flex flex-col {{ sm:flex-col xs:flex-col md:flex-row lg:flex-row }}  gap-x-8 overflow-y-auto pb-10 gap-y-16 lg:mx-8 xl:mx-12 xl:gap-x-16">
