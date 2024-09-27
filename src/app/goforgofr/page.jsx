85e8b71bcaa28ce99cb987f67a7d236b6c49504e
satvik-shukla-zs <satvik.shukla@zopsmart.com>
2024-09-27 12:20:57 +0530

    [ CHANGES ] : added GitHub star

--- src/app/goforgofr/page.jsx
+++ src/app/goforgofr/page.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+"use client"
+import React, {useEffect, useState} from 'react';
 import GFGSponsor from "@/components/goforgofr/GFGSponsor";
 import GFGFaq from "@/components/goforgofr/GFGFaq";
 import GFGHero from "@/components/goforgofr/GFGHero";
@@ -6,45 +7,29 @@ import GFGServices from "@/components/goforgofr/GFGServices";
 import GFGTracks from "@/components/goforgofr/GFGTracks";
 import GFGHackathon from "@/components/goforgofr/GFGHackathon";
 import GFGPrizes from "@/components/goforgofr/GFGPrizes";
+import GFGHeader from "@/components/goforgofr/GFGHeader";
 
 const Page = () => {
+
   return (
     <>
       <link href="https://fonts.cdnfonts.com/css/impact" rel="stylesheet" />
       <div className={`flex flex-col gap-4`}>
-        <div className={`flex items-center gap-4 p-4`}>
-          <a
-            aria-label="Home page"
-            className="flex items-center gap-2 text-3xl font-bold"
-            href="/"
-          >
-            <span className="italic text-sky-400">Go</span>
-            <span className="not-italic text-slate-700 dark:text-white">
-              Fr
-            </span>
-          </a>
+        <GFGHeader />
 
-          <div className={`ml-16 items-center gap-4 md:gap-16 font-semibold hidden md:flex`}>
-            <a href={'#'}>Schedule</a>
-            <a href={'#sponsors_section'} className={`min-w-[120px]`}>Our sponsors</a>
-            <a href={'#'}>Prizes</a>
-            <a href={'#faq_section'}>Faq</a>
-          </div>
-        </div>
+        <GFGHero/>
 
-        <GFGHero />
+        <GFGServices/>
 
-        <GFGServices />
+        <GFGTracks/>
 
-        <GFGTracks />
+        <GFGHackathon/>
 
-        <GFGHackathon />
+        <GFGPrizes/>
 
-        <GFGPrizes />
+        <GFGSponsor/>
 
-        <GFGSponsor />
-
-        <GFGFaq />
+        <GFGFaq/>
       </div>
     </>
   )
---
+++ src/components/goforgofr/GFGHeader.jsx
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from 'react'
+import Link from "next/link";
+import {GitHubIcon} from "@/components/Layout";
+import {formatNumber} from "@/lib/common";
+
+const GfgHeader = () => {
+  const [githubStars, setGithubStars] = useState(null)
+
+  useEffect(() => {
+    const fetchRepoData = async () => {
+      try {
+        const response = await fetch(
+          'https://api.github.com/repos/gofr-dev/gofr',
+        )
+        if (!response.ok) {
+          throw new Error('Network response was not ok')
+        }
+        const data = await response.json()
+        if (data?.watchers) setGithubStars(data?.watchers)
+      } catch (error) {
+        console.error('Error fetching the repo data:', error)
+      }
+    }
+    fetchRepoData()
+  }, [])
+
+  return (
+    <div className={`flex w-full justify-between items-center p-4`}>
+      <div className={`flex items-center gap-4`}>
+        <a
+          aria-label="Home page"
+          className="flex items-center gap-2 text-3xl font-bold"
+          href="/"
+        >
+          <span className="italic text-sky-400">Go</span>
+          <span className="not-italic text-slate-700 dark:text-white">Fr</span>
+        </a>
+
+        <div
+          className={`ml-16 hidden items-center gap-4 font-semibold md:flex md:gap-16`}
+        >
+          <a href={'#'}>Schedule</a>
+          <a href={'#sponsors_section'} className={`min-w-[120px]`}>
+            Our sponsors
+          </a>
+          <a href={'#'}>Prizes</a>
+          <a href={'#faq_section'}>Faq</a>
+        </div>
+      </div>
+
+      <Link
+        href="https://github.com/gofr-dev/gofr"
+        className="group"
+        target="_blank"
+        aria-label="GitHub"
+      >
+        <div className="group relative rounded-xl border border-slate-200 dark:border-slate-800">
+          <div className="absolute -inset-px rounded-xl border-2 border-transparent opacity-10 [background:linear-gradient(var(--quick-links-hover-bg,theme(colors.sky.50)),var(--quick-links-hover-bg,theme(colors.sky.50)))_padding-box,linear-gradient(to_top,theme(colors.indigo.400),theme(colors.cyan.400),theme(colors.sky.500))_border-box] group-hover:opacity-100 dark:[--quick-links-hover-bg:theme(colors.slate.800)]" />
+          <div className="relative flex items-center justify-center space-x-1 overflow-hidden rounded-xl px-2 py-1">
+            <GitHubIcon className="mr-1 h-5 w-5 fill-slate-400 group-hover:fill-slate-500 dark:group-hover:fill-slate-300" />
+
+            {githubStars && (
+              <div className="animate-fadeIn flex items-center gap-1">
+                <span className=" text-sm font-semibold text-white">
+                  {formatNumber(githubStars)}
+                </span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="#fcd34d"
+                  className="h-3.5 w-3.5 [--icon-background:theme(colors.white)] [--icon-foreground:theme(colors.slate.900)]"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+            )}
+          </div>
+        </div>
+      </Link>
+    </div>
+  )
+}
+
+export default GfgHeader
