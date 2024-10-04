3c15119c4965eea0bae523a112d12f0789f8f22d
satvik-shukla-zs <satvik.shukla@zopsmart.com>
2024-10-04 12:57:49 +0530

    [ CHANGE ] : code refactor

--- src/components/goforgofr/GFGHero.jsx
+++ src/components/goforgofr/GFGHero.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import {Button} from "@/components/Button";
+import { Button } from '@/components/Button'
 
 import GO_FOR_GOFr from '@/images/goforgofr/GO_FOR_GOFr.svg'
 import Hackathon from '@/images/goforgofr/Hackathon.svg'
 import HeroSvg from '@/images/goforgofr/HeroSvg.svg'
-import Image from "next/image";
-import Link from "next/link";
-import {HeroBackground} from "@/components/HeroBackground";
-import blurCyanImage from "@/images/blur-cyan.png";
+import Image from 'next/image'
+import Link from 'next/link'
+import { HeroBackground } from '@/components/HeroBackground'
 
 const GfgHero = () => {
   const gradientText = {
@@ -19,44 +18,71 @@ const GfgHero = () => {
   }
 
   return (
-    <div className={`flex items-center justify-center md:py-20 px-10 md:px-14 md:pl-20 flex-col-reverse md:flex-row relative`}>
-      <HeroBackground className="absolute right-0 translate-x-1/2 top-1/2 -translate-y-1/2 lg:translate-y-[-60%] z-0 opacity-30" />
-      <Image
-          className="absolute top-[-100px] left-0 opacity-50 z-0"
-          src={blurCyanImage}
-          alt=""
-          width={530}
-          height={530}
+    <div
+      className={`relative flex flex-col-reverse items-center justify-center px-10 md:flex-row md:px-14 md:py-20 md:pl-20`}
+    >
+      <HeroBackground className="absolute right-0 top-1/2 z-0 -translate-y-1/2 translate-x-1/2 opacity-30 lg:translate-y-[-60%]" />
+      <img
+        className="absolute left-0 top-[-100px] z-0 opacity-50"
+        src={'/img/hackathon/blur-cyan-min.png'}
+        alt=""
+        width={530}
+        height={530}
       />
 
-        <Image
-            className="absolute -right-2 -top-64 opacity-50 z-0"
-            src={blurCyanImage}
-            alt=""
-            width={530}
-            height={530}
-        />
-      <div className={`flex w-full max-w-[300px] md:max-w-full md:w-1/2 justify-center flex-col`}>
-        <div className={`flex flex-col gap-4 py-10 z-20`}>
+      <img
+        className="absolute -right-2 -top-64 z-0 opacity-50"
+        src={'/img/hackathon/blur-cyan-min.png'}
+        alt=""
+        width={530}
+        height={530}
+      />
+      <div
+        className={`flex w-full max-w-[300px] flex-col justify-center md:w-1/2 md:max-w-full`}
+      >
+        <div className={`z-20 flex flex-col gap-4 py-10`}>
           <div className={`text-6xl font-semibold`}>
-            <Image src={GO_FOR_GOFr} alt={"Go for GoFr"} className={`max-w-[400px] w-full`} />
+            <Image
+              src={GO_FOR_GOFr}
+              alt={'Go for GoFr'}
+              className={`w-full max-w-[400px]`}
+            />
           </div>
           <div className={`text-6xl font-semibold`}>
-            <Image src={Hackathon} alt={"Go for GoFr"} className={`max-w-[400px] w-full`} />
+            <Image
+              src={Hackathon}
+              alt={'Go for GoFr'}
+              className={`w-full max-w-[400px]`}
+            />
           </div>
           <div>
-            <span className={`italic font-semibold`}>Innovate , Code, Create & Elevate</span>
+            <span className={`font-semibold italic`}>
+              Innovate , Code, Create & Elevate
+            </span>
           </div>
           <div className={`mt-6`}>
-            <Link href={'https://docs.google.com/forms/d/e/1FAIpQLSeAlrrQNyCYgEhbBtN5X-y97YzEXpNVM7RLNdtAvhgYMiBysA/viewform'} target={'_blank'}>
+            <Link
+              href={
+                'https://docs.google.com/forms/d/e/1FAIpQLSeAlrrQNyCYgEhbBtN5X-y97YzEXpNVM7RLNdtAvhgYMiBysA/viewform'
+              }
+              target={'_blank'}
+            >
               <Button className={` !px-8`}>Register Now</Button>
             </Link>
           </div>
         </div>
       </div>
 
-      <div className={`flex w-full max-w-[300px] md:max-w-full md:w-1/2 items-center justify-end`}>
-        <Image src={HeroSvg} alt={"Go for GoFr"} width={50} height={50} className={`z-10 w-full max-w-[500px] `} />
+      <div
+        className={`flex w-full max-w-[300px] items-center justify-end md:w-1/2 md:max-w-full`}
+      >
+        <Image
+          src={HeroSvg}
+          alt={'Go for GoFr'}
+          width={50}
+          height={50}
+          className={`z-10 w-full max-w-[500px] `}
+        />
       </div>
     </div>
   )
