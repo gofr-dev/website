59b4e58aa1b47f615e80944be1c1adb5743fe12f
Satyam Trivedi <112492501+satyamt05@users.noreply.github.com>
2025-02-19 18:52:08 +0530

    fixed responsiveness

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -9,7 +9,7 @@ export default function Testimonials() {
             {/*<h2 className="text-center text-lg font-semibold leading-8 text-white mb-10">*/}
             {/*    User Testimonials*/}
             {/*</h2>*/}
-            <div className="columns-3">
+            <div className="md:columns-3">
                 {testimonials.map((testimonial, index) => (
                     <div key={index} className="group inline-block relative my-3 rounded-xl border cursor-default border-slate-200 dark:border-slate-800 p-1">
                         <div className="absolute -inset-px rounded-xl border-2 border-transparent opacity-0 [background:linear-gradient(var(--quick-links-hover-bg,theme(colors.sky.50)),var(--quick-links-hover-bg,theme(colors.sky.50)))_padding-box,linear-gradient(to_top,theme(colors.indigo.400),theme(colors.cyan.400),theme(colors.sky.500))_border-box] group-hover:opacity-100 dark:[--quick-links-hover-bg:theme(colors.slate.800)]" />
