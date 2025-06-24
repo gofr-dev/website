9662821f872a0afdea8878316ca26da3c3f66c50
Ari1009 <arihant0pal@gmail.com>
2025-06-24 19:14:04 +0530

    fix: add link container

--- src/components/QuickLinks.jsx
+++ src/components/QuickLinks.jsx
@@ -9,8 +9,15 @@ export function QuickLinks({ children }) {
 }
 
 export function QuickLink({ title, description, icon, href }) {
+  const Container = href ? 'a' : 'div';
+  
   return (
-    <a href={href} className="group relative rounded-xl border border-slate-200 dark:border-slate-800 block no-underline">
+    <Container 
+      href={href}
+      className={`group relative rounded-xl border border-slate-200 dark:border-slate-800 block ${
+        href ? 'no-underline hover:cursor-pointer' : ''
+      }`}
+    >
       <div className="absolute -inset-px rounded-xl border-2 border-transparent opacity-0 [background:linear-gradient(var(--quick-links-hover-bg,theme(colors.sky.50)),var(--quick-links-hover-bg,theme(colors.sky.50)))_padding-box,linear-gradient(to_top,theme(colors.indigo.400),theme(colors.cyan.400),theme(colors.sky.500))_border-box] group-hover:opacity-100 dark:[--quick-links-hover-bg:theme(colors.slate.800)]" />
       <div className="relative overflow-hidden rounded-xl p-6">
         <Icon icon={icon} className="h-8 w-8" />
@@ -21,6 +28,6 @@ export function QuickLink({ title, description, icon, href }) {
           {description}
         </p>
       </div>
-    </a>
+    </Container>
   );
 }
