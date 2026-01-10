31aa96360c9c1b069c9b864704f4b03c823ec948
Divya Darshana <98943137+coolwednesday@users.noreply.github.com>
2026-01-10 14:23:36 +0530

    Added extension for SQS module (#207)

---
+++ src/app/pkg/gofr/datasource/pubsub/sqs/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Page = () => {
+    return (
+        <div className={`flex justify-center items-center min-h-[70vh]`}>
+            <div className={`text-4xl font-semibold flex gap-3 flex-wrap justify-center`}>
+                <span>
+                </span>
+                <span className={`text-sky-400`}></span>
+            </div>
+        </div>
+    );
+};
+
+export default Page;
