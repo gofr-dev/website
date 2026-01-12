1508e5baa42e38dff4214bb3065e50d7e3401a07
devanshzop <devansh.arora@zop.dev>
2026-01-12 18:49:52 +0530

    added mail icon

---
+++ src/components/icons/EmailIcon.jsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+const EmailIcon = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="currentColor"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0z"></path>
+    <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0z"></path>
+  </svg>
+);
+
+export default EmailIcon;
